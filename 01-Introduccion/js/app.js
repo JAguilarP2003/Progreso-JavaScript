bf5aa244f2fbff3a5b3728661fb95a38a36e2f13
Juan Aguilar <jdavid2003@gmail.com>
2021-04-01 23:19:19 -0500

    Agregando evidencias de las primeras 4 secciones.

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,23 @@
+/*
+    ========================================
+    === LE PREGUNTA AL USUARIO SU NOMBRE ===
+    ========================================
+*/
+const nombre = prompt('Cuál es tu nonbre?');
+
+// Toma el nombre y lo muestra en pantalla.
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+
+// Asigna el valor hacia la variable producto.
+const producto ='Monitor 24 pulgadas';
+
+
+console.log("Hola");
+console.log('Mundo');
+
+
+function hola() {
+    console.log('ok');
+    console.log("Hola");
+    console.log('Mundo');
+}
---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,15 @@
+const producto = 'Monitor de 20 Pulgadas';
+
+console.log(producto);
+
+// Conocer la cantidad de letras de la cadena de texto
+console.log(producto. length);
+
+
+
+//console.log(roducto.indexOf('Tablet'));
+
+
+console.log(producto.includes('Tablet'));
+console.log(producto.includes('Monitor'));
+console.log(producto.includes('monitor'));

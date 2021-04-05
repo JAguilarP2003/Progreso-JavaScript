da7e8b9abcdfbd239e5a10007e86bafac717a0b6
Juan Aguilar <jdavid2003@gmail.com>
2021-04-05 16:23:03 -0500

    Añadiendo seccón 6 terminada

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+const autenticado = false;
+
+// if(autenticado) {
+//     console.log("Sí puedes ver Netflix");
+// }else{
+//     console.log("No puedes verlo");
+// }
+
+
+// Operador ternario
+
+console.log(autenticado ? "Sí está autenticado" : "No está autenticado");

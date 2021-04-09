1e4febb675ef551f3b4b47864cf00d3f5bf7c9a2
Juan Aguilar <jdavid2003@gmail.com>
2021-04-08 21:41:49 -0500

    Agregando sección 8, primer ejercicio.

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,13 @@
+const numeros = [5, 40, 12];
+
+const meses = new Array('Enero', 'Abril', 'Octubre');
+
+console.log(numeros);
+console.log(meses);
+
+
+// Un arreglo que contiene datos de todo tipo.
+
+const deTodo = ["12 horas",  25, false, "si", null, {nombre: "Juan", cargo: "Aprendíz"}, [1,2,3]];
+
+console.log(deTodo);

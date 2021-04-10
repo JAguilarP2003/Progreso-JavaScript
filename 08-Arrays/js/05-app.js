16b35dc28de87f91c87864b5e8c210819d51e1b2
Juan Aguilar <jdavid2003@gmail.com>
2021-04-10 18:53:38 -0500

    Agregando ejercicios 2-5, sección 8.

---
+++ 08-Arrays/js/05-app.js
@@ -0,0 +1,24 @@
+const carrito = [];
+
+// Definir un producto.
+const producto = {
+    nombre: "Teclado Tenkeyless",
+    valor: 499.99
+}
+
+const producto2 = {
+    nombre: "Mouse Logitech G203",
+    valor: 199.85
+}
+
+carrito.push(producto);
+carrito.push(producto2);
+
+const producto3 = {
+    nombre: "Monitor 144Hz",
+    valor: 500
+}
+
+carrito.unshift(producto3)
+
+console.table(carrito);

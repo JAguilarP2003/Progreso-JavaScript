b87a4988e00427bb3dbd641e603ce9cdab6c99bc
Juan Aguilar <jdavid2003@gmail.com>
2021-04-19 21:13:40 -0500

    Agregando ejercicios 6 y 7, sección 10.

---
+++ 10-Estructuras/js/06-app.js
@@ -0,0 +1,15 @@
+const usuario = true;
+const puedePagar = true;
+
+if (usuario && puedePagar) {
+    console.log('Sí puedes comprar.');
+
+} else if (!puedePagar && !usuario) {
+    console.log('Fondos insuficientes.');
+
+} else if (!usuario) {
+    console.log('Inicia sesión primero.');
+
+} else if (!puedePagar) {
+    console.log('Fondos insuficientes.');
+}

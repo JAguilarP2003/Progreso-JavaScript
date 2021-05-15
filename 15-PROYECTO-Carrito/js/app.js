6e01ca7b811c4363ecc9565465d80ad3d6c403c4
Juan Aguilar <jdavid2003@gmail.com>
2021-05-14 22:29:03 -0500

    Añadiendo versión 2, proyecto 1, video 116.

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,3 +1,5 @@
+// 13/05/2021
+
 // Variables
 const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
@@ -15,7 +17,34 @@ function registrarEventListeners() {
 // Funciones
 function agregarCurso(cheems) {
     cheems.preventDefault(); // Esta sintaxis evita que la página vuelva hacia arriba.
+
     if (cheems.target.classList.contains('agregar-carrito')) {
-        console.log(cheems.target);   
+
+
+        const cursoSeleccionado = cheems.target.parentElement.parentElement;
+
+
+        datosCurso(cheems.target.parentElement.parentElement);
     }  
 };
+
+
+// 14/05/2021
+
+// Realizará un Traversing para obtener la iformación del curso.
+
+function datosCurso(curso) {
+    // console.log(curso);
+
+
+    // Crear un Objeto con el contenido del curso seleccionado.
+    const info = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    console.log(info);
+}

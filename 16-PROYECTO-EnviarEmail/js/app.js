0b84937adac44b03760ee61d04373478e99d033a
Juan Aguilar <jdavid2003@gmail.com>
2021-05-19 20:55:48 -0500

    Agregando versión 2, proyecto 2, video 125 - Validar los campos.

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -3,13 +3,26 @@
 
 // Variables
 
-eventListeners();
+
 
 const enviar = document.querySelector('#enviar')
 
 
+// Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const msg = document.querySelector('#mensaje');
+
+eventListeners();
 function eventListeners() {
+    // Una vez arranque la APP
     document.addEventListener('DOMContentLoaded', iniciar)
+
+    // Campos del formulario
+    email.addEventListener('blur', validarForm);
+    asunto.addEventListener('blur', validarForm);
+    msg.addEventListener('blur', validarForm);
+
 }
 
 
@@ -18,4 +31,14 @@ function eventListeners() {
 function iniciar() {
     enviar.disabled = true;
     enviar.classList.add('cursor-notallowed', 'opacity-50'); // Añade clases del Framework CSS Tailwind
+}
+
+
+// Valida el formulario (Dependiedo de los datos en el input, así mismo serán sus clases.)
+function validarForm(cheems) {
+    if (cheems.target.value.length > 0) {
+        console.log('Sí hay algo');
+    } else {
+        cheems.target.classList.add('border', 'border-red-500');
+    }
 }

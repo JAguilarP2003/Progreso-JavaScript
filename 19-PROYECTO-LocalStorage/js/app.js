e759f0e1cb9adafffa3f6f982a3b1d31d5c45587
Juan Aguilar <jdavid2003@gmail.com>
2021-05-28 20:59:50 -0500

    Agregando versión 2, proyecto 4, video 146 - Validando Formulario.

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -18,5 +18,30 @@ function eventListeners() {
 function addTweet(cheems) {
     cheems.preventDefault();
 
-    console.log('Adding tweet');
+    // Text Area donde el usuario escribe.
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validación.
+    if (tweet === '') {
+        showError('You have to write something.');
+
+        return; // Evita que se ejecuten más líneas de código.
+    }
+
+    console.log('Agregando tweet');
+}
+
+// Mostrar Mensaje de Error
+function showError(error) {
+    const msgError = document.createElement('p');
+    msgError.textContent = error;
+    msgError.classList.add('error');
+
+    // Insertarlo en el contenido.
+    const content = document.querySelector('#contenido');
+    content.appendChild(msgError);
+
+    setTimeout(() => {
+        msgError.remove(); // Elimina la alerta después de 3 segundos.
+    }, 3000);
 }

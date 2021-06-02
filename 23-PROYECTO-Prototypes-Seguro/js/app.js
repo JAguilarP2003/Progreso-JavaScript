2f2324a164d3e9443091bc0ff4f79f094219c245
Juan Aguilar <jdavid2003@gmail.com>
2021-06-01 22:09:56 -0500

    Agregando versión 3, proyecto 6, video 161 - Prototype para mostrar un error.

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -26,6 +26,28 @@ UI.prototype.fillOptions = () => {
     }
 }
 
+// Muestra alertas en pantalla.
+UI.prototype.showMsg = (msg, type) => {
+    const div = document.createElement('div');
+
+    if (type === 'error') {
+        div.classList.add('mensaje', 'error')
+    } else {
+        div.classList.add ('mensaje', 'correcto');
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = msg;
+
+    // Insertar en el HTML.
+    const form = document.querySelector('#cotizar-seguro');
+    form.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+}
+
 // Instanciar UI
 const ui = new UI();
 
@@ -52,8 +74,13 @@ function quoteInsurance(cheems) {
     const type = document.querySelector('input[name="tipo"]:checked').value;
     
     if (brand === '' || year === '' || type === '') {
-        console.log('No pasó la validación.');
-    } else {
-        console.log('Sí pasó la validación');
+        ui.showMsg('Todos los campos son obligatorios.', 'error');
+        return;
     }
+    ui.showMsg('Cotizando...', 'exito');
+
+    // Instanciar el Seguro.
+    
+
+    // Utilizar el protoype que va a cotizar.
 }

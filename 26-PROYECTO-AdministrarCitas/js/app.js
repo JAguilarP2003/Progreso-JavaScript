9fa3e401601e8feceb64f1aaff5cd92c852f59dd
Juan Aguilar <jdavid2003@gmail.com>
2021-06-12 19:05:02 -0500

    Agregando versión 2, proyecto 8, video 180 - Validación y msg error.

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -11,21 +11,59 @@ const symptomsInput = document.querySelector('#sintomas');
 
 // UI.
 const form = document.querySelector('#nueva-cita');
-const ctrAppointment = document.querySelector('#citas');
+const ctrAppt = document.querySelector('#citas');
+
+class Appts {
+    constructor(){
+        this.appts = [];
+    }
+}
+
+class UI {
+    printAlert(msg, type) {
+        // Crear el div.
+        const divMsg = document.createElement('div');
+        divMsg.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        // Agregar clase en base al tipo de error.
+        if (type === 'error') {
+            divMsg.classList.add('alert-danger');
+        } else {
+            divMsg.classList.add('alert-success');
+        }
+
+        // Mensaje de error.
+        divMsg.textContent = msg;
+
+        // Agregar al DOM.
+        document.querySelector('#contenido').insertBefore(divMsg, document.querySelector('.agregar-cita'));
+
+        // Quitar alerta.
+        setTimeout(() => {
+            divMsg.remove();
+        }, 5000);
+    }
+}
+
+const ui = new UI();
+
+const manageAppts = new Appts();
 
 // Registrar eventos.
 eventListeners();
 function eventListeners() {
-    petInput.addEventListener('input', dataAppointment);
-    ownerInput.addEventListener('input', dataAppointment);
-    telInput.addEventListener('input', dataAppointment);
-    dateInput.addEventListener('input', dataAppointment);
-    hourInput.addEventListener('input', dataAppointment);
-    symptomsInput.addEventListener('input', dataAppointment);
+    petInput.addEventListener('input', dataAppt);
+    ownerInput.addEventListener('input', dataAppt);
+    telInput.addEventListener('input', dataAppt);
+    dateInput.addEventListener('input', dataAppt);
+    hourInput.addEventListener('input', dataAppt);
+    symptomsInput.addEventListener('input', dataAppt);
+
+    form.addEventListener('submit', newAppt)
 }
 
 // Objeto con la información de la cita.
-const appointmentObj = {
+const apptObj = {
     pet: '',
     owner: '',
     tel: '',
@@ -35,7 +73,20 @@ const appointmentObj = {
 }
 
 // Agrega datos al objeto de la cita.
-function dataAppointment(cheems) {
-    appointmentObj[cheems.target.name] = cheems.target.value;
-    console.log(appointmentObj);
+function dataAppt(cheems) {
+    apptObj[cheems.target.name] = cheems.target.value;
+}
+
+// Valida y agrega una nueva cita a la clase de citas.
+function newAppt(cheems) {
+    cheems.preventDefault();
+
+    // Extraer la información del objeto de cita.
+    const {pet, owner, tel, date, hour, symptoms} = apptObj;
+
+    // Validar.
+    if (pet == '' || owner == '' || tel == '' || date == '' || hour == '' || symptoms == '') {
+        ui.printAlert('Todos los campos son obligatorios.', 'error');
+        return;
+    }
 }

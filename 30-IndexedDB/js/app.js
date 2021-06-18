85e7e736c6b6db449fe0aa5b67c2ab2ed5dce8d2
Juan Aguilar <jdavid2003@gmail.com>
2021-06-18 16:37:02 -0500

    Agregando ejercicio 2, sección 30.

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -17,8 +17,20 @@ function crmDB() {
     }
 
     // Configuración de la base de datos.
-    crmDB.onupgradeneeded = function () {
-        console.log('Este método sólo se ejecuta una vez.');
+    crmDB.onupgradeneeded = function (e) {
+       const db = e.target.result;
+
+       const objectStore = db.createObjectStore('crm', {
+           keyPath: 'crm',
+           autoIncrement: true
+       });
+
+       // Definir las columnas.
+       objectStore.createIndex('nombre', 'nombre', {unique:false});
+       objectStore.createIndex('email', 'email', {unique:true});
+       objectStore.createIndex('tel', 'tel', {unique:true});
+
+       console.log('Columnas creadas');
     }
 
 }

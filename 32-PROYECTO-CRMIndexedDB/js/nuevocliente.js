7fd96bdd108b84f9ade8dd1441de310d96d57780
Juan Aguilar <jdavid2003@gmail.com>
2021-06-22 18:08:45 -0500

    Agregando versión 2, proyecto 10, video 212 - Primeros pasos.

---
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -0,0 +1,66 @@
+(function () {
+    let DB;
+    const form = document.querySelector('#formulario');
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        conectDB();
+
+        form.addEventListener('submit', validateCustomer);
+    });
+
+    function conectDB() {
+        const openConection = window.indexedDB.open('crm', 1);
+
+        openConection.onerror = function () {
+            console.log('Hubo un error');
+        };
+
+        openConection.onsuccess = function () {
+            DB = openConection.result;
+        }
+    }
+
+    function validateCustomer(cheems) {
+        cheems.preventDefault();
+
+        // Leer todos los inputs.
+        const name = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const tel = document.querySelector('#telefono').value;
+        const company = document.querySelector('#empresa').value;
+
+        if (name === '' || email === '' || tel === '' || company === '') {
+            printAlert('Todos los campos son obligatorios', 'error');
+
+            return;
+        }
+    }
+
+    function printAlert(msg, type) {
+        
+        const alert = document.querySelector('.alerta');
+
+        if (!alert) { 
+            // Crear la alerta.
+            const divMsg = document.createElement('div');
+            divMsg.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mt-6', 'text-center', 'border', 'alerta');
+
+            if (type === 'error') {
+                divMsg.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+            } else {
+                divMsg.classList.add('bg-green-100', ' border-green-400', 'text-green-700');
+            }
+
+            divMsg.textContent = msg;
+
+            form.appendChild(divMsg);
+
+            setTimeout(() => {
+                divMsg.remove();
+            }, 3000);
+        }
+    }
+
+})();

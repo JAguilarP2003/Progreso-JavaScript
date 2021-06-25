201d748b7b633f7a27d1744c8f9771b84a9780e0
Juan Aguilar <jdavid2003@gmail.com>
2021-06-24 21:03:06 -0500

    Agregando versión 4, proyecto 10, video 214 - Query a la DB.

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -6,6 +6,10 @@
 
     document.addEventListener('DOMContentLoaded',() => {
         createDB();
+
+        if (window.indexedDB.open('crm', 1)) {
+            getCustomers();
+        }
     });
 
     // Crea la base de datod de IndexDB.
@@ -35,4 +39,50 @@
             console.log('DB lista y creada.');
         }
     }
+
+    function getCustomers() {
+        const openConnection = window.indexedDB.open('crm', 1);
+
+        openConnection.onerror = function () {
+            console.log('Hubo un error');
+        }
+
+        openConnection.onsuccess = function () {
+            DB = openConnection.result;
+
+            const objectStore = DB.transaction('crm').objectStore('crm');
+
+            objectStore.openCursor().onsuccess = function (cheems) {
+                const cursor = cheems.target.result;
+
+                if (cursor) {
+                    const { name, company, email, tel, id } = cursor.value;
+
+                    const customerList = document.querySelector('#listado-clientes');
+                    customerList.innerHTML += ` 
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${name} </p>
+                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                <p class="text-gray-700">${tel}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                <p class="text-gray-600">${company}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            </td>
+                        </tr>
+                    `;
+                    cursor.continue();
+                } else {
+                    console.log('No hay más registros.');
+                }
+            }
+        }
+    }
+
 })();

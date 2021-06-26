09793cc0ac5fed1a4755f4df91f87b68815d17b3
Juan Aguilar <jdavid2003@gmail.com>
2021-06-25 19:05:42 -0500

    Agregando versión 6, proyecto 10, video 216 - Llenar formulario.

--- 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/editarcliente.js
@@ -1,20 +1,66 @@
 (function () {
     let DB;
+    let customerId;
 
     const inputName = document.querySelector('#nombre');
+    const inputEmail = document.querySelector('#email');
+    const inputTel = document.querySelector('#telefono');
+    const inputCompany = document.querySelector('#empresa');
+
+    const form = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
         conectDB();
 
+        // Actualiza el registro.
+        form.addEventListener('submit', updateCustomer);
+
         // Verificar el id de la URL.
         const paramURL = new URLSearchParams(window.location.search);
-        const customerId = paramURL.get('id');
+        customerId = paramURL.get('id');
         if (customerId) {
             setTimeout(() => {
                 getCustomer(customerId);
-            }, 50);
+            }, 15);
         }
     });
+
+    function updateCustomer(cheems) {
+        cheems.preventDefault();
+
+        if (inputName.value === '' || inputEmail.value === '' || inputTel.value === '' || inputCompany.value === '') {
+            printAlert('Todos los campos son obligarotios.', 'error');
+
+            return;
+        }
+
+        //Actualizar cliente.
+        const updatedCustomer = {
+            name: inputName.value,
+            email: inputEmail.value,
+            tel: inputEmail.value,
+            company: inputCompany.value,
+            id: Number(customerId)
+        }
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.put(updatedCustomer);
+       
+        transaction.oncomplete = function () {
+            printAlert('Editado correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+
+        transaction.onerror = function () {
+            printAlert('Hubo un error', 'error');
+        }
+
+    }
 
     function getCustomer(id) {
         const transaction = DB.transaction(['crm'], 'readonly');
@@ -35,9 +81,12 @@
     }
 
     function fillForm(dataCustomer) {
-        const { name } = dataCustomer;
+        const { name, email, tel, company } = dataCustomer;
 
         inputName.value = name;
+        inputEmail.value = email;
+        inputTel.value = tel;
+        inputCompany.value = company;
     }
 
     function conectDB() {
--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -10,17 +10,6 @@
         form.addEventListener('submit', validateCustomer);
     });
 
-    function conectDB() {
-        const openConection = window.indexedDB.open('crm', 1);
-
-        openConection.onerror = function () {
-            console.log('Hubo un error');
-        };
-
-        openConection.onsuccess = function () {
-            DB = openConection.result;
-        }
-    }
 
     function validateCustomer(cheems) {
         cheems.preventDefault();
@@ -72,29 +61,5 @@
         }
     }
 
-    function printAlert(msg, type) {
-        
-        const alert = document.querySelector('.alerta');
-
-        if (!alert) { 
-            // Crear la alerta.
-            const divMsg = document.createElement('div');
-            divMsg.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mt-6', 'text-center', 'border', 'alerta');
-
-            if (type === 'error') {
-                divMsg.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
-            } else {
-                divMsg.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
-            }
-
-            divMsg.textContent = msg;
-
-            form.appendChild(divMsg);
-
-            setTimeout(() => {
-                divMsg.remove();
-            }, 3000);
-        }
-    }
 
 })();

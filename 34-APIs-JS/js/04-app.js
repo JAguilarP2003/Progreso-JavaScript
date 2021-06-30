247744fa54cbcb3a65ee2647867bc217883accf6
Juan Aguilar <jdavid2003@gmail.com>
2021-06-29 20:22:53 -0500

    Agregando ejercicio 4, sección 34 - Ejecutar pantalla completa.

---
+++ 34-APIs-JS/js/04-app.js
@@ -0,0 +1,13 @@
+const openBtn = document.querySelector('#abrir-pantalla-completa');
+const exitBtn = document.querySelector('#salir-pantalla-completa');
+
+openBtn.addEventListener('click', fullScreen);
+exitBtn.addEventListener('click', exitFullScreen);
+
+function fullScreen() {
+    document.documentElement.requestFullscreen();
+}
+
+function exitFullScreen() {
+    document.exitFullscreen();
+}

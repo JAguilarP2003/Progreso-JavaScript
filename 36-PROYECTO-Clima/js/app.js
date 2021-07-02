51a4ec481a77c55e29c6bc74d194fb0309260479
Juan Aguilar <jdavid2003@gmail.com>
2021-07-01 20:55:48 -0500

    Agregando versión 1, sección 32, video 233 - Primeros pasos.

---
+++ 36-PROYECTO-Clima/js/app.js
@@ -0,0 +1,17 @@
+// 01/07/2021 - Fecha de Inicio.
+
+const container = document.querySelector('.container');
+const reult = document.querySelector('#resultado');
+const form = document.querySelector('#formulario');
+
+window.addEventListener('load', ()=> {
+    form.addEventListener('submit', searchWeather);
+})
+
+function searchWeather(cheems) {
+    cheems.preventDefault();
+
+    // Validar
+
+    // Consultar la API
+}

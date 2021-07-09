befbae10ea8a9fd787fa9b1249b10451e039fef7
Juan Aguilar <jdavid2003@gmail.com>
2021-07-08 21:54:42 -0500

    Agregando Versión 2, Proyecto 14, Video 245 - Mostrar Resultados de la Consulta.

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -51,7 +51,7 @@ function showAlert(msg) {
 function searchImgs(term) {
     const key = '22421346-17d124c6dd18b79b45c27098e';
 
-    const url = `https://pixabay.com/api/?key=${key}&q=${term}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${term}&per_page=100`;
 
     fetch(url)
         .then(answer => answer.json())
@@ -62,4 +62,44 @@ function searchImgs(term) {
 
 function showImgs(imgs) {
     console.log(imgs);
+
+    while (result.firstChild) {
+        result.removeChild(result.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imágenes y construir el HTML.
+    imgs.forEach(img => {
+        const {previewURL, likes, views, largeImageURL} = img;
+
+        result.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold"> 
+                            ${likes}
+                            <span class="font-light">
+                                Me gusta
+                            </span>
+                        </p>
+
+                        <p class="font-bold"> 
+                            ${views}
+                            <span class="font-light">
+                                Visualizaciones
+                            </span>
+                        </p>
+
+                        <a
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p1"
+                            href=${largeImageURL} target="_blank" rel="noopener noreferrer">
+                                Ver imágen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
 }

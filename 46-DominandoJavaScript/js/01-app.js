752e4057cc6492d9d5da49b84cd468e878fb7c1a
Juan Aguilar <jdavid2003@gmail.com>
2021-08-02 21:40:23 -0500

    Agregando Ejercicio 1, Sección 46, Video 287 - Scope.

---
+++ 46-DominandoJavaScript/js/01-app.js
@@ -0,0 +1,22 @@
+// const customer = 'Juan';
+
+// function showCustomer () {
+//     const customer = 'David';
+//     console.log(customer);
+// }
+
+// showCustomer();
+
+const login = true;
+
+const customer = 'Juan';
+
+function loggedInCustomer() {
+}
+
+function function2() {
+    console.log(customer);
+}
+
+function2();
+loggedInCustomer();

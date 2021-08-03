1de90090ced844903eeca29b3bbccd581d9695a9
Juan Aguilar <jdavid2003@gmail.com>
2021-08-02 21:57:01 -0500

    Agregando Ejercicio 3, Sección 46, Video 289 - Coercion.

---
+++ 46-DominandoJavaScript/js/03-app.js
@@ -0,0 +1,14 @@
+const number1 = 20;
+const number2 = "21";
+
+console.log(number1 + number2); // Implícita.
+
+console.log(Number(number2)); // Explícita.
+
+console.log(number1.toString());
+
+const order = [1,2,3,4];
+
+console.log(order.toString());
+
+console.log(JSON.stringify(order));

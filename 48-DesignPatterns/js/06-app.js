b35fcc5a33395b8218da0c8f5c05b73362806c04
Juan Aguilar <jdavid2003@gmail.com>
2021-08-12 21:28:50 -0500

    Agregando Ejercicio 6, Sección 48, Video 310 - Mixin Pattern.

--- 48-DesignPatterns/js/06-app.js
+++ 48-DesignPatterns/js/06-app.js
@@ -1,3 +1,38 @@
-console.log(module1.name);
+class Person {
+    constructor (name, email) {
+        this.name = name;
+        this.email = email;
+    }
+}
 
-module1.hello();
+class Customer {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+}
+
+const personFunctions = {
+    showInfo() {
+        console.log(`- Name: ${this.name} - Email: ${this.email} -`)
+    },
+    showName() {
+        console.log(`- My name is: ${this.name} -`);
+    }
+}
+
+// Añadir personfunctions a la clase Person.
+Object.assign(Person.prototype, personFunctions);
+Object.assign(Customer.prototype, personFunctions);
+
+const customer = new Person ('Juan', 'juan@gmail.com');
+
+console.log(customer);
+customer.showInfo();
+customer.showName();
+
+const customer2 = new Customer('Customer', 'customer@gmail.com');
+
+console.log(customer2);
+customer2.showInfo();
+customer2.showName();
